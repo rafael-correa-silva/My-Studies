9e1bb0225afb4c8a0f0af89b71a1e8cb6af4e10c
Rafael Corrêa Silva <rafaelcorsil2006@gmail.com>
2026-06-01 01:26:50 -0300

    organizando as pastas

---
+++ hora-de-codar-js/6_string/JS07.js
@@ -0,0 +1,11 @@
+var nome = "Matheus";
+var sobreNome = "Santos";
+
+var nomeCompleto = nome + " " + sobreNome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log("Seu nome é: " + nomeCompleto + ". 'Será?'"); // PAra escrever uma palavra e deixar ela entre aspas as aspas dela deve ser diferente da aspas usada para escrever a frase. Ex: " ' " ou ' " ' ou ` " ` ou ` ' `
---
+++ hora-de-codar-js/7_boolean/js/scripts.js
@@ -0,0 +1,7 @@
+var verdadeiro = true;
+var falso = false;
+console.log(verdadeiro);
+console.log(falso);
+console.log(typeof verdadeiro);
+console.log(typeof falso);
+
